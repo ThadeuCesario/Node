36f44b41ec7d3e60101e549c57527ea8a70f9440
Thadeu Munhóz Cesário <thadeu.cesario@icloud.com>
2021-09-24 07:54:00 -0300

    working with exports and imports modules in NodeJS

---
+++ Cod3r/Módulos/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA');
+const moduloB = require('./moduloB');
+
+console.log("MóduloA", moduloA);
+console.log(moduloA.ola);
+console.log(moduloA.bemVindo);
+console.log(moduloA.ateLogo);
+console.log("----------------");
+console.log("MóduloB", moduloB);
+console.log(moduloB.bomDia);
+console.log(moduloB.boaNoite());
